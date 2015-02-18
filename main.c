fea82003a6c97c926af12a34869de8f1d3047046
Leandro Passetti <leandropassetti@gmail.com>
2015-02-18 00:08:23 -0300

    Se agrego el menu con sus respectivas opciones, para el envio por puerto serie, codigo funcionando, con algunos pequeños detalles de visualizacion

--- main.c
+++ main.c
@@ -8,13 +8,20 @@
 #define OFF 0
 #define ON 1
 #define LUZ_APAGADA 2000
+#define TEMPMAX 25
+#define TEMPMIN 16
+#define HUMMIN 80
+#define HUMMAX 60
+#define LUZENC 60
+#define LUZAPA 60
 
-void imprimir_menu(void);
+uint16_t imprimir_menu(void);
 
 uint32_t count = 0; //contador que se incrementa cada un segundo
 uint16_t datoRecibido;
 uint8_t flagRx = 0;
 uint8_t botonPresionado = 0;
+char opcion;
 
 int main(void) {
 
@@ -43,6 +50,8 @@ int main(void) {
 
 	uint8_t flagE = 0;
 	uint8_t flagA = 0;
+
+	uint16_t seteo;
 
 //inicializo hardware
 
@@ -130,19 +139,57 @@ int main(void) {
 
 		}
 
-
 		if (luz < LUZ_APAGADA) {
 			if (encenderLuz == 1)
-								//alarma();
+				//alarma();
 				led_on(2);
 			else
 				led_off(2);
 		}
 
-		if(botonPresionado == 1){
+		if (botonPresionado == 1) {
 
 			botonPresionado = 0;
-			imprimir_menu();
+			flagRx = 0;
+			seteo = imprimir_menu();
+
+			if(seteo != 0)
+			{
+				switch(opcion){
+
+				case '2':
+					tempMin = (float) seteo;
+					break;
+				case '3':
+					tempMax = (float) seteo;
+					break;
+				case '4':
+					humMin = (float) seteo;
+					break;
+				case '5' :
+					humMax = (float) seteo;
+					break;
+				case '6' :
+					luzEncendida = (uint32_t) (seteo * 3600); //pasar horas a segundos
+					break;
+				case '7' :
+					luzApagada = (uint32_t) (seteo * 3600);
+					break;
+				case '8' :
+					tempMin = TEMPMIN;
+					tempMax = TEMPMAX;
+					humMin = HUMMIN;
+					humMax = HUMMAX;
+					luzEncendida = LUZENC;
+					luzApagada = LUZAPA;
+					break;
+
+				}
+
+
+
+			}
+
 
 		}
 
@@ -176,33 +223,186 @@ void APP_ISR_sw(void) {
 	botonPresionado = 1;
 }
 
-void imprimir_menu(void){
+uint16_t imprimir_menu(void) {
 
-	char stringMenu[30];
-		float temp, hum;
-		char opcion;
+	char stringMenu[50];
+	float temp, hum;
+	uint16_t espera;
+	uint16_t valorDevolver;
+	uint8_t dato[4] = {0};
+	uint8_t i;
 
-		temp = leer_temperatura();
-		hum = leer_humedad();
+	temp = leer_temperatura();
+	hum = leer_humedad();
 
-		sprintf(stringMenu, "\n \rEste va a ser el menu\n \r");
+	sprintf(stringMenu, "\n \rIngrese la opcion que desea:\n \r");
+	transmit_string(stringMenu);
+	sprintf(stringMenu, "1. Humedad y temperatura actual\n \r");
+	transmit_string(stringMenu);
+	sprintf(stringMenu, "2. Setear temperatura minima \n \r");
+	transmit_string(stringMenu);
+	sprintf(stringMenu, "3. Setear temperatura máxima\n \r");
+	transmit_string(stringMenu);
+	sprintf(stringMenu, "4 . Setear humedad mínima\n \r");
+	transmit_string(stringMenu);
+	sprintf(stringMenu, "5. Setear humedad máxima\n \r");
+	transmit_string(stringMenu);
+	sprintf(stringMenu, "6. Setear tiempo de encendido de luz\n \r");
+	transmit_string(stringMenu);
+	sprintf(stringMenu, "7. Setear tiempo de apagado de luz\n \r");
+	transmit_string(stringMenu);
+	sprintf(stringMenu, "8. Valores por defecto \n \r");
+	transmit_string(stringMenu);
+
+	espera = count;
+	while (flagRx == 0) {
+		if (espera >= (count + 20))
+			return 0;
+	}
+
+	opcion = datoRecibido;
+
+	switch (opcion) {
+	case '1':
+		sprintf(stringMenu, "Temperatura = %2f °C \n \r", temp);
 		transmit_string(stringMenu);
-		sprintf(stringMenu, "1. humedad \n \r");
-		transmit_string(stringMenu);
-		sprintf(stringMenu, "2. temperatura \n \r");
+		sprintf(stringMenu, "Humedad = %2f '%' \n \r", hum);
 		transmit_string(stringMenu);
 
+		valorDevolver = 2;
 
-		sprintf(stringMenu, "2. temperatura = %f \n \r", temp);
+		break;
+
+	case '2':
+		sprintf(stringMenu, "ingrese el valor de temperatura minima\n \r");
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "valor comprendido entre 10°C y 30°C\n \r");
+		transmit_string(stringMenu);
+		espera = count;
+		for (i = 0; i < 2; i++) {
+			while (flagRx == 0) {
+				if (espera >= (count + 20))
+					return 0;
+			}
+			dato[i] = (uint8_t) datoRecibido;
+		}
+
+		valorDevolver = ((dato[0] * 10) + dato[1]);
+
+		break;
+
+	case '3':
+		sprintf(stringMenu, "ingrese el valor de temperatura maxima\n \r");
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "valor comprendido entre 20°C y 40°C\n \r");
+		transmit_string(stringMenu);
+		espera = count;
+		for (i = 0; i < 2; i++) {
+			while (flagRx == 0) {
+				if (espera >= (count + 20))
+					return 0;
+			}
+			dato[i] = (uint8_t) datoRecibido;
+		}
+
+		valorDevolver = ((dato[0] * 10) + dato[1]);
+
+		break;
+
+	case '4':
+		sprintf(stringMenu, "ingrese el valor de humedad minima\n \r");
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "valor comprendido entre 20% - 60%\n \r");
+		transmit_string(stringMenu);
+		espera = count;
+		for (i = 0; i < 2; i++) {
+			while (flagRx == 0) {
+				if (espera >= (count + 20))
+					return 0;
+			}
+			dato[i] = (uint8_t) datoRecibido;
+		}
+
+		valorDevolver = ((dato[0] * 10) + dato[1]);
+
+		break;
+	case '5':
+		sprintf(stringMenu, "ingrese el valor de humedad máxima\n \r");
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "valor comprendido entre 50% y 100%\n \r");
+		transmit_string(stringMenu);
+		espera = count;
+		for (i = 0; i < 2; i++) {
+			while (flagRx == 0) {
+				if (espera >= (count + 20))
+					return 0;
+			}
+			dato[i] = (uint8_t) datoRecibido;
+		}
+
+		valorDevolver = ((dato[0] * 10) + dato[1]);
+
+		break;
+	case '6':
+		sprintf(stringMenu, "ingrese el  tiempo de encendido de luz\n \r");
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "valor comprendido entre 1 y 12 hs\n \r");
+		transmit_string(stringMenu);
+		espera = count;
+		for (i = 0; i < 2; i++) {
+			while (flagRx == 0) {
+				if (espera >= (count + 20))
+					return 0;
+			}
+			dato[i] = (uint8_t) datoRecibido;
+		}
+
+		valorDevolver = ((dato[0] * 10) + dato[1]);
+
+		break;
+
+	case '7':
+		sprintf(stringMenu, "ingrese el tiempo de apagado de luz\n \r");
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "valor comprendido entre 1 y 12 hs\n \r");
 		transmit_string(stringMenu);
 
+		espera = count;
+		for (i = 0; i < 2; i++) {
+			while (flagRx == 0) {
+				if (espera >= (count + 20))
+					return 0;
+			}
+			dato[i] = (uint8_t) datoRecibido;
+		}
 
+		valorDevolver = ((dato[0] * 10) + dato[1]);
 
-		sprintf(stringMenu, "1. humedad = %f \n \r", hum);
+		break;
+
+	case '8':
+		sprintf(stringMenu, "se setearan los valores por defecto\n \r");
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "temperatura entre %d y %d\n \r", TEMPMIN, TEMPMAX);
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "humedad entre %d y %d\n \r", HUMMIN, HUMMAX);
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "tiempo de encendido = %d segundos\n \r", LUZENC);
+		transmit_string(stringMenu);
+		sprintf(stringMenu, "tiempo de apagado = %d segundos\n \r", LUZAPA);
 		transmit_string(stringMenu);
 
+		return 1;
 
+		break;
+
+	default:
+		sprintf(stringMenu, "opcion incorrecta, presione menu nuevamente\n \r");
+		transmit_string(stringMenu);
+		break;
+	}
+
+	return valorDevolver;
 
 }
-
 
